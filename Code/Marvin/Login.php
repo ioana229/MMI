a479756f1a2f6e14da2d9d661e975ae3e5f241fa
馬文 <Marvin.Inkermann@bs-erlangen.de>
2025-02-05 09:56:55 +0100

    Update Login.php

--- Code/Marvin/Login.php
+++ Code/Marvin/Login.php
@@ -1,1 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""
+// $dbname needs to be changed befor staging
+$dbname = "food_db_test";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
